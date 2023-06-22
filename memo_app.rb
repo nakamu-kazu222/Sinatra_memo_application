b73d90e48d7cef05e7492addb3559fd0561bab5f
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-06-22 23:01:31 +0900

    データの保存先をPostgreSQLに変更した

--- memo_app.rb
+++ memo_app.rb
@@ -5,8 +5,19 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 require 'rack/session/cookie'
+require 'pg'
+
+configure do
+  conn = PG.connect(dbname: 'postgres', user: 'postgres')
+  result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+  conn.exec('CREATE TABLE memos (id UUID PRIMARY KEY, title varchar(255), text text)') if result.values.empty?
+end
 
 helpers do
+  def conn
+    @conn ||= PG.connect(dbname: 'postgres', user: 'postgres')
+  end
+
   def memo_data_json_file_path(id)
     "json/memos_#{id}.json"
   end
@@ -19,21 +30,33 @@ helpers do
     SecureRandom.uuid
   end
 
-  def get_memo(id)
-    memo_file_path = memo_data_json_file_path(id)
-    return nil unless File.exist?(memo_file_path)
+  def db_connection
+    yield(conn)
+  ensure
+    conn&.close
+  end
 
-    memo_content = File.read(memo_file_path)
-    JSON.parse(memo_content, symbolize_names: true)
+  def get_memo(id)
+    result = conn.exec_params('SELECT * FROM memos WHERE id = $1', [id])
+    result[0]&.transform_keys(&:to_sym) unless result.count.zero?
   end
 
   def save_memo(memo)
-    File.open(memo_data_json_file_path(memo[:id]), 'w') { |file| JSON.dump(memo, file) }
+    db_connection do |conn|
+      if memo['id'].nil?
+        conn.exec_params('INSERT INTO memos (id, title, text) VALUES ($1::uuid, $2, $3) ON CONFLICT (id) DO UPDATE SET title = $2, text = $3',
+                         [memo[:id], memo[:title], memo[:text]])
+      else
+        conn.exec_params('UPDATE memos SET title = $1, text = $2 WHERE id = $3', [memo[:title], memo[:text], memo[:id]])
+      end
+    end
   end
-end
 
-before do
-  FileUtils.mkdir_p('json') unless Dir.exist?('json')
+  def delete_memo(id)
+    db_connection do |conn|
+      conn.exec_params('DELETE FROM memos WHERE id = $1', [id])
+    end
+  end
 end
 
 get '/' do
@@ -41,7 +64,9 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = Dir.glob('json/*.json').map { |file| JSON.parse(File.read(file), symbolize_names: true) }
+  @memos = []
+  result = conn.exec('SELECT * FROM memos')
+  @memos = result.map { |data| data.transform_keys(&:to_sym) }
   erb :index
 end
 
@@ -88,7 +113,7 @@ patch '/memos/:id' do
 end
 
 delete '/memos/:id' do
-  File.delete(memo_data_json_file_path(params[:id]))
+  delete_memo(params[:id])
   redirect to('/memos')
 end
 
